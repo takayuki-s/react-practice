f0fe52b638e63c95fa384e4b9e9aba2a97ace20b
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-05-21 00:08:11 +0900

    React.Fragment省略型

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDom from "react-dom";
 
 const App = () => {
   return (
-    <React.Fragment>
+    <>
       <h1>こんにちは</h1>
       <p>お元気ですか？</p>
-    </React.Fragment>
+    </>
   );
 };
 
