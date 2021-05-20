bba488a4654a421512b962c24163fd80e10fa002
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-05-21 00:07:40 +0900

    React.Fragmentで1つの要素にまとめる

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDom from "react-dom";
 
 const App = () => {
-  return null;
+  return (
+    <React.Fragment>
+      <h1>こんにちは</h1>
+      <p>お元気ですか？</p>
+    </React.Fragment>
+  );
 };
 
 ReactDom.render(<App />, document.getElementById("root"));
