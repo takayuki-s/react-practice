f87002c4388e9d8bb0540f442606484ed7894698
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-05-22 11:52:11 +0900

    propsを分割代入でコードをリファクタリング

--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 const ColorfulMessage = (props) => {
+  const { color, children } = props;
   const contentStyle = {
-    color: props.color,
+    color: color,
     fonsSize: "18px"
   };
-  return <p style={contentStyle}>{props.children}</p>;
+  return <p style={contentStyle}>{children}</p>;
 };
 
 export default ColorfulMessage;
